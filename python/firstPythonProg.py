01b65be0b8f7041db0e93ba3cd2d9022db89a08c
v0lsh3bnik <hello@volshebnik.xyz>
2020-05-27 07:59:10 +0000

    init commit

---
+++ python/firstPythonProg.py
@@ -0,0 +1,13 @@
+# saluer user
+print("Hello cher utilisateur")
+# demander son nom
+username = input("Quel est votre nom ? ")
+# afficher son nom
+print('Bonjour ' + username + ", vous etes beau!")
+# demander l'age
+user_age = int(input("Quel est votre age ? "))
+# afficher l'age
+print("vous avez : " + str(user_age) + " ans")
+# donner son age dans 5 ans
+user_age_cinq_ans = user_age + 5
+print("Vous aurez : " + str(user_age_cinq_ans) + " dans cinq ans")
