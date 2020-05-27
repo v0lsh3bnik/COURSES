cb9735f7120ddcc94a9154c185e258ced5af3949
v0lsh3bnik <hello@volshebnik.xyz>
2020-05-27 08:37:18 +0000

    edits for first python prog

--- python/firstPythonProg.py
+++ python/firstPythonProg.py
@@ -1,13 +1,13 @@
 # saluer user
-print("Hello cher utilisateur")
-# demander son nom
-username = input("Quel est votre nom ? ")
+print("Hello user !")
+# demander le nom de l'utilisateur
+username = input("quel est votre nom?: ")
 # afficher son nom
-print('Bonjour ' + username + ", vous etes beau!")
-# demander l'age
-user_age = int(input("Quel est votre age ? "))
-# afficher l'age
-print("vous avez : " + str(user_age) + " ans")
-# donner son age dans 5 ans
-user_age_cinq_ans = user_age + 5
-print("Vous aurez : " + str(user_age_cinq_ans) + " dans cinq ans")
+print("Salut " + username + ", vous etes beau")
+# demander son age
+user_age = int(input("Quel est votre age?: "))
+# afficher son age
+print("Vous avez : " + str(user_age) + " an(s)")
+# ajouter plus 5 a son age
+user_age_plus_cinq = user_age + 5
+print("vous aurez : " + str(user_age_plus_cinq) + " dans 5 ans")
